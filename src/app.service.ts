fead2b7c58650806f441323e3daa88c7387cda84
sebas350 <profesebastian8@gmail.com>
2025-04-18 02:58:46 -0300

    ok fetch

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'hola mundo';
+  getHello(): string[] {
+    return ['hola Nest', 'hola kiti', 'nada'];
   }
 }
